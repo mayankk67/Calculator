72461644357a5e2a6dc5dc85d95f27ec17bfbb00
mayankk67 <mayankk67@gmail.com>
2019-11-20 20:57:07 +0530

    done

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import rootReducer from './Reducers/rootReducers';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const store=createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
